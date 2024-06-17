2e621f0501cc48983a123bc39bb1d667b11e575c
Káposzta Gergő <gergo.kaposzta@kifli.tech>
2024-06-17 23:33:51 +0200

    style improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import SectionLayout from '@components/SectionLayout/SectionLayout.tsx';
 export default function Page() {
   return (
     <SectionLayout>
-      <h1 className={'text-2xl font-semibold'}>B2i Healthcare Homework</h1>
-      <h2 className={'text-sm'}>SNOMED CT Quick Search Component</h2>
+      <h1 className={'text-2xl font-semibold text-gray-800'}>B2i Healthcare Homework</h1>
+      <h2 className={'text-sm text-gray-800'}>SNOMED CT Quick Search Component</h2>
     </SectionLayout>
   );
 }
